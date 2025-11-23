4e04a956ae11b143ca21eab7c005c53fa769b150
Carlos Barreto <contato.cerb@outlook.com>
2025-11-23 16:31:41 -0300

    `feat::`add serializers, urls and views

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Users
+from .serializers import UsersSerializer
 
-# Create your views here.
+class UsersAPIView(APIView):
+    def get(self, request):
+        users = Users.objects.all();
+        serializer = UsersSerializer(users, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = UsersSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
